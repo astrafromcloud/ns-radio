2998f415495e5f6bc04cb9d3af66ef4f517adc76
Rome <zhailauramazan@gmail.com>
2024-11-04 18:20:31 +0500

    updated ContactSeeder.php

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ContactResource extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return $this->collection->transform(function ($contact) {
+            return [
+                'phones' => json_decode($contact->phones),
+                'description' => $contact->description,
+                'address' => $contact->address,
+                'email' => $contact->email,
+                'instagram_url' => $contact->instagram_url,
+                'youtube_url' => $contact->youtube_url,
+                'whatsapp_url' => $contact->whatsapp_url,
+                'telegram_url' => $contact->telegram_url,
+            ];
+        })->toArray();
+
+    }
+}
