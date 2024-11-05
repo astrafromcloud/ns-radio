b4ddffb083ddbbf18a13a28abb4764e9e47aaf86
Rome <zhailauramazan@gmail.com>
2024-11-05 19:57:50 +0500

    fix: liveseeder

--- database/seeders/LiveTranslationSeeder.php
+++ database/seeders/LiveTranslationSeeder.php
@@ -13,7 +13,7 @@ class LiveTranslationSeeder extends Seeder
     public function run(): void
     {
         $liveTranslations = [
-            'video_url' => '`<iframe src="https://vk.com/video_ext.php?oid=-204003801&id=456305602&hd=2&autoplay=1" width="853" height="480" allow="autoplay; encrypted-media; fullscreen; picture-in picture; screen-wake-lock;" frameborder="0" allowfullscreen></iframe>`'
+            'video_url' => '<iframe src="https://vk.com/video_ext.php?oid=-21723674&id=456239124&hd=2&autoplay=1" width="853" height="480" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0" allowfullscreen></iframe>'
         ];
 
         LiveTranslation::create($liveTranslations);
