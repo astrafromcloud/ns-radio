a95c630ab250c6f83a3ff3c818e115e500cefaa3
Rome <zhailauramazan@gmail.com>
2024-11-06 15:20:22 +0500

    fix: seeders

--- database/seeders/BroadcasterProgramSeeder.php
+++ database/seeders/BroadcasterProgramSeeder.php
@@ -15,7 +15,9 @@ class BroadcasterProgramSeeder extends Seeder
     public function run(): void
     {
         $broadcaster = Broadcaster::find(1);
-        $broadcaster->programs()->attach([1, 2, 3]);
+        $broadcaster->programs()->attach([1, 2]);
+        $broadcaster2 = Broadcaster::find(1);
+        $broadcaster2->programs()->attach([2, 3]);
 //        BroadcasterProgram::find(1);
     }
 }
